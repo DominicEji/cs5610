8420f456e7fdd43b8076ea56dae1f3de6dd5d1c1
DominicEji <dominic.ejiogu@gmail.com>
2025-01-30 14:13:01 -0800

    I have also added a code using while loop to log powers of 2 from 2 to 1024

--- week4/js/script.js
+++ week4/js/script.js
@@ -4,3 +4,10 @@ for (let i = 2; i <= 1024; i *= 2) {
     console.log(i);
 }
 
+// Using a while loop to log powers of 2 from 2 to 1024
+console.log("Powers of 2 using while loop:");
+let num = 2;
+while (num <= 1024) {
+    console.log(num);
+    num *= 2;
+}
