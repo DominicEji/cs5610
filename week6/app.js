d50c0dbd6dcfb4176dad604d395d35491651942a
DominicEji <dominic.ejiogu@gmail.com>
2025-02-13 13:52:47 -0800

    I made editing to the code by adding codes that defined the message to be written to the file, and used fs.write to write the message.

--- week6/app.js
+++ week6/app.js
@@ -1,30 +1,12 @@
 const fs = require('fs');
 
-const filePath = 'message.txt';
 const message = 'Hello, this is a message written using Node.js!';
 
 // Write to a file (creates the file if it doesn’t exist)
-fs.writeFile(filePath, message, (err) => {
+fs.writeFile('output.txt', message, 'utf8', (err) => {
     if (err) {
         console.error('Error writing file:', err);
         return;
     }
-    console.log('Message written successfully!');
+    console.log('File written successfully!');
 
-    // Read the file after writing
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error('Error reading file:', err);
-            return;
-        }
-        console.log('Message read from file:', data);
-    });
-});
-
-const logger = require('./logger.js'); // Import the logger module
-
-// Call the log function from logger.js
-logger.log();
-
-// Print the version from logger.js
-console.log('Logger version:', logger.version);
