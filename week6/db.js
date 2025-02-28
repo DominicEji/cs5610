98a5b4cffc6c85947bf3930ad30861ab63670c7a
DominicEji <dominic.ejiogu@gmail.com>
2025-02-27 16:42:12 -0800

    I updated  dotenv implementation.

--- week6/db.js
+++ week6/db.js
@@ -1,6 +1,7 @@
 const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
-const url = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER_URL}/cs5610?retryWrites=true&w=majority`;;
+const url = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER_URL}/cs5610?retryWrites=true&w=majority&appName=FreeCluster`;
 const client = new MongoClient(url);
 
 async function connectToDatabase() {
