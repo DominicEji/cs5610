848ab28e2afc63becc3e340fc895c617a31dbf94
DominicEji <dominic.ejiogu@gmail.com>
2025-03-13 15:15:11 -0700

    I updated code with proper alignment

--- week9/react-project/src/components/Header.jsx
+++ week9/react-project/src/components/Header.jsx
@@ -2,8 +2,9 @@ function Header({ appName }) {
     return (
       <header>
         <h1>{appName}</h1>
-        <button>Add a Task</button>
+        <button>Add A Task</button>
       </header>
     );
   }
+  
   export default Header;
