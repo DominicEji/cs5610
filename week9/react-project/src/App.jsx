038002f9594d3de031dbb04d4fc761c0e85c8d04
DominicEji <dominic.ejiogu@gmail.com>
2025-03-13 14:21:13 -0700

    I updated code that imports the Header component

---
+++ week9/react-project/src/App.jsx
@@ -0,0 +1,10 @@
+import Header from './components/Header';
+function App() {
+  const welcomeMessage = "Welcome to My App";
+  return (
+    <div>
+      <h1>{welcomeMessage}</h1>
+    </div>
+  );
+}
+export default App;
