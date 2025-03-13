9b4e1fa146800158260430de61b5933581ebc51b
DominicEji <dominic.ejiogu@gmail.com>
2025-03-13 15:29:02 -0700

    I updated code to include task array. i.e. i added the hadr-coded array

--- week9/react-project/src/App.jsx
+++ week9/react-project/src/App.jsx
@@ -1,10 +1,32 @@
 import Header from './components/Header';
+
 function App() {
-  const welcomeMessage = "Welcome to My App";
+  const appName = "Welcome to My App";
+
+  // Hard-coded tasks array
+  const tasks = [
+    {
+      id: 1,
+      title: "Review week 9 material",
+      date: "June 4th at 1 pm",
+    },
+    {
+      id: 2,
+      title: "Do quiz 9",
+      date: "June 4th at 6 pm",
+    },
+    {
+      id: 3,
+      title: "Work on assignment 2",
+      date: "June 5th at 8 am",
+    },
+  ];
+  
   return (
     <div>
-      <h1>{welcomeMessage}</h1>
+      <Header appName={appName} />
     </div>
   );
 }
+
 export default App;
