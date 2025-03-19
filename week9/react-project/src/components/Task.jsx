235351282a94b2f3e80ea77d77cdcc39513ae5fe
DominicEji <dominic.ejiogu@gmail.com>
2025-03-18 17:27:44 -0700

    I propely formatted code

--- week9/react-project/src/components/Task.jsx
+++ week9/react-project/src/components/Task.jsx
@@ -1,19 +1,19 @@
 import { FaTrash } from 'react-icons/fa';
 
 function Task({ task }) {
-    return (
-      <li>
-        <div className="task-item">
-          <div className="task-content">
+  return (
+    <li>
+      <div className="task-item">
+        <div className="task-content">
           <p><strong>{task.title}</strong></p>
           <p>{task.date}</p>
         </div>
         <div className="task-actions">
           <FaTrash className="delete-icon" />
         </div>
-        </div>
-      </li>
-    );
-  }
-  
-  export default Task;
+      </div>
+    </li>
+  );
+}
+
+export default Task;
