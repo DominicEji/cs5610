b38faa172f12a5783f26733a3321d63a2f58a1f5
DominicEji <dominic.ejiogu@gmail.com>
2025-03-18 17:26:35 -0700

    I imported a delete icon from react icons and added a <div> inside the existing <div> to create a flex container for icon and task text

--- week9/react-project/src/components/Task.jsx
+++ week9/react-project/src/components/Task.jsx
@@ -1,9 +1,16 @@
+import { FaTrash } from 'react-icons/fa';
+
 function Task({ task }) {
     return (
       <li>
         <div className="task-item">
+          <div className="task-content">
           <p><strong>{task.title}</strong></p>
           <p>{task.date}</p>
+        </div>
+        <div className="task-actions">
+          <FaTrash className="delete-icon" />
+        </div>
         </div>
       </li>
     );
