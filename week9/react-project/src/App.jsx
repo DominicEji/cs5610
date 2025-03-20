ff12f1b11ec7194238db9ec889bd9e0b3bd129f7
DominicEji <dominic.ejiogu@gmail.com>
2025-03-19 23:14:04 -0700

    I used the useState hook to create a state variable (showAddTask) to track whether the form should be shown or hidden.

--- week9/react-project/src/App.jsx
+++ week9/react-project/src/App.jsx
@@ -1,14 +1,24 @@
+import React, { useState } from 'react';
 import Header from './components/Header';
 import TasksList from './components/TasksList';
 import AddTask from './components/AddTask';
 
 function App() {
   const appName = "Welcome to My App";
+  const [showAddTask, setShowAddTask] = useState(false);
+
+  const toggleAddTask = () => {
+    setShowAddTask(!showAddTask);
+  };
 
   return (
     <div>
-      <Header appName={appName} />
-      <AddTask />
+      <Header
+        appName={appName}
+        showAddTask={showAddTask}
+        onToggleAddTask={toggleAddTask}
+      />
+      {showAddTask && <AddTask />} {/* Conditionally render AddTask */}
       <TasksList />
     </div>
   );
