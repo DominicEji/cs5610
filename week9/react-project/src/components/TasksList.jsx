7d52a62735275ef4e58648aa5342b09b5dfc1a3a
DominicEji <dominic.ejiogu@gmail.com>
2025-03-20 18:07:06 -0700

    I modified TasksList.jsx to accept tasks as a prop and render them.

--- week9/react-project/src/components/TasksList.jsx
+++ week9/react-project/src/components/TasksList.jsx
@@ -1,37 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Task from './Task';
 
-function TasksList() {
-    // Hard-coded tasks array
-    const [tasks, setTasks] = useState([
-      {
-        id: 1,
-        title: "Review week 9 material",
-        date: "June 4th at 1 pm",
-      },
-      {
-        id: 2,
-        title: "Do quiz 9",
-        date: "June 4th at 6 pm",
-      },
-      {
-        id: 3,
-        title: "Work on assignment 2",
-        date: "June 5th at 8 am",
-      },
-    ]);
-
-  // Updated with function to handle task deletion
-  const handleDelete = (id) => {
-    setTasks(tasks.filter((task) => task.id !== id));
-  };
-
+function TasksList({ tasks, onDelete }) {
   return (
     <div>
       {tasks.length > 0 ? (
         <ul>
           {tasks.map((task) => (
-            <Task key={task.id} task={task} onDelete={handleDelete} />
+            <Task key={task.id} task={task} onDelete={onDelete} />
           ))}
         </ul>
       ) : (
