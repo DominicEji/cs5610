dfa9b01d8ccbe2606ab1efc94a316be8cd1bd860
DominicEji <dominic.ejiogu@gmail.com>
2025-03-20 17:07:35 -0700

    I used useEffect to fetch data when the component mounts

--- week9/react-project/src/App.jsx
+++ week9/react-project/src/App.jsx
@@ -3,9 +3,26 @@ import Header from './components/Header';
 import TasksList from './components/TasksList';
 import AddTask from './components/AddTask';
 
+
 function App() {
   const appName = "Welcome to My App";
   const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([])
+
+  // Fetch tasks from the fake server
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/tasks');
+        const data = await response.json();
+        setTasks(data); // Update state with fetched tasks
+      } catch (error) {
+        console.error('Error fetching tasks:', error);
+      }
+    };
+
+    fetchTasks(); // Call the async function
+  }, []); 
 
   const toggleAddTask = () => {
     setShowAddTask(!showAddTask);
